f62b49138d999e28f654c5c797236e191570f8a0
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-11-06 13:12:36 -0600

    router and models

--- server/router/router.go
+++ server/router/router.go
@@ -1,1 +1,18 @@
 package router
+
+import (
+	"github.com/gorilla/mux"
+	"totolist/server/middleware"
+)
+
+func Router() *mux.Router {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/task", middleware.GetAllTasks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/tasks", middleware.CreateTask).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/tasks/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/undoTask/{id}", middleware.UndoTask).Methods("PUTS", "OPTIONS")
+	router.HandleFunc("/api/deleteTask{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/deleteAllTasks", middleware.deleteAllTasks).Methods("DELETE", "OPTIONS")
+	return router
+}
