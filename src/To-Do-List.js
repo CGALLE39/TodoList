5e14cf48212f8f62b331b1f9eea1997e5b930e2d
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-11-06 12:07:08 -0600

    server

---
+++ src/To-Do-List.js
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+import axios from "axios";
+import {Card, Header, Form, Input, Icon} from "semantic-ui-react";
+
+let endpoint = "http/localhost:9000";
+
+class ToDoList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.stte = {
+            task:"",
+            items:[],
+        };
+    }
+    ComponentDidMount(){
+        this.getTask();
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <Header className="header" as="h2" color="yellow">
+                        To Do List
+                    </Header>
+                </div>
+                <div className="row">
+                    <form onSubmit={this.onSubmit}>
+                        <Input
+                        type={"text"}
+                        name={"task"}
+                        onChange={this.onChange}
+                        value={this.state.task}
+                        fluid
+                        placeholder={"Create Task"}
+                        />
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ToDoList;
