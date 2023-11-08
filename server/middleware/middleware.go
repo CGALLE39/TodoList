34838180cc805fba95c4f6af89ce8e900480ee7f
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-11-08 14:33:31 -0600

    structure

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,1 +1,45 @@
 package middleware
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"net/http"
+)
+
+var collection *mongo.Collection
+
+func init() {
+	loadTheEnv()
+	createDBInstance()
+}
+
+func loadTheEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading the .env file")
+	}
+}
+
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateTask() {
+
+}
+
+func TaskComplete() {
+
+}
+
+func UndoTask() {
+
+}
+
+func DeleteTask() {
+
+}
+
+func DeleteAllTasks() {
+
+}
