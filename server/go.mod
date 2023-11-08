8ef39b455b36ac56b1c2dc64ea763710463ddcc4
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-11-08 15:33:17 -0600

    env connection

---
+++ server/go.mod
@@ -0,0 +1,15 @@
+module totolist
+
+go 1.21.2
+
+require (
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/xdg/scram v1.0.5 // indirect
+	github.com/xdg/stringprep v1.0.3 // indirect
+	go.mongodb.org/mongo-driver v1.0.0 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,9 +1,14 @@
 package middleware
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
+	"os"
 )
 
 var collection *mongo.Collection
@@ -20,8 +25,34 @@ func loadTheEnv() {
 	}
 }
 
-func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+func createDBInstance() {
+	connectionString := os.Getenv("DB_URI")
+	dbName := os.Getenv("DB_NAME")
+	collName := os.Getenv("DB_COLLECTION_NAME")
 
+	clientOptions := options.Client().ApplyURL(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected to mongodb!")
+
+	collection = client.Database(dbName).Collection(collName)
+
+	fmt.Println("collection instance created")
+}
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	payload := GetAllTasks()
+	json.NewEncoder(w).Encode(payload)
 }
 
 func CreateTask() {
